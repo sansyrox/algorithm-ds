f0d8920efa063d04962432db3939c2fe60d9cd82
Sanskar Jethi <sansyrox@gmail.com>
2019-07-10 00:09:46 +0530

    Updated

---
+++ leet_code/domin_rotation.cpp
@@ -0,0 +1,14 @@
+ public int minDominoRotations(int[] A, int[] B) {
+        int n = A.length;
+        for (int i = 0, a = 0, b = 0; i < n && (A[i] == A[0] || B[i] == A[0]); ++i) {
+            if (A[i] != A[0]) a++;
+            if (B[i] != A[0]) b++;
+            if (i == n - 1) return Math.min(a, b);
+        }
+        for (int i = 0, a = 0, b = 0; i < n && (A[i] == B[0] || B[i] == B[0]); ++i) {
+            if (A[i] != B[0]) a++;
+            if (B[i] != B[0]) b++;
+            if (i == n - 1) return Math.min(a, b);
+        }
+        return -1;
+    }
