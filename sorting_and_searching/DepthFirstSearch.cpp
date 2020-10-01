3e975a263462431d305ac5ac6d4096aa80979d85
saurabhatram <44276174+saurabhatram@users.noreply.github.com>
2020-10-01 20:15:55 +0530

    Update and rename DepyhFirstSearch.cpp to DepthFirstSearch.cpp

--- sorting_and_searching/DepthFirstSearch.cpp
+++ sorting_and_searching/DepthFirstSearch.cpp
@@ -66,24 +66,7 @@ void Graph::DFS(int v)
 	DFSUtil(v, visited); 
 } 
 
-// // Driver code 
-// int main() 
-// { 
-// 	// Create a graph given in the above diagram 
-// 	Graph g(4); 
-// 	g.addEdge(0, 1); 
-// 	g.addEdge(0, 2); 
-// 	g.addEdge(1, 2); 
-// 	g.addEdge(2, 0); 
-// 	g.addEdge(2, 3); 
-// 	g.addEdge(3, 3); 
-
-// 	cout << "Following is Depth First Traversal"
-// 			" (starting from vertex 2) \n"; 
-// 	g.DFS(2); 
-
-// 	return 0; 
-// } 
+ 
 
 int main() 
 { 
