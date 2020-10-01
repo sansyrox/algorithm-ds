757d17be5a3bcb8074bf6242b87e925351ba75b0
SHARIF NAWAZ <sharifnawaz747@gmail.com>
2020-10-01 20:37:40 +0530

    Add files via upload
    Problem Name: Longest Palindrome
    Problem LInk: https://codeforces.com/contest/1304/problem/B
    #Accepted Solution in Python3

---
+++ codeforces/Longest Palindrome.py
@@ -0,0 +1,26 @@
+a=input()
+a=a.split()
+n,m=[int(i) for i in a]
+li=[]
+p=[]
+for i in range(n):
+    x=input()
+    if(x==x[::-1]):
+        p.append(x)
+    else:
+        li.append(x)
+result=[]
+s=""
+l=""
+for j in li:
+    if(j[::-1] in li):
+        s=s+j
+        l=j[::-1]+l
+        li.pop(li.index(j[::-1]))
+if (len(p)):
+    print(len(s+p[0]+l))
+    print(s+p[0]+l)
+else:
+    print(len(s+l))
+    print(s+l)
+
