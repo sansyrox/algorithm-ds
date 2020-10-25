116a43510aca49573e262d7c5785977714949f07
Yash Gupta <31829813+yash04g@users.noreply.github.com>
2020-10-26 02:41:36 +0530

    BellmanFord Algorithm Implementation

---
+++ graph/BellmanFord.cpp
@@ -0,0 +1,70 @@
+// Its the implementation of Bellman Ford Algorithm
+#include <bits/stdc++.h>
+#include<unordered_map>
+#include<unordered_set>
+using namespace std;
+
+#define int long long
+#define ff first
+#define ss second
+#define loop(i,k,n) for(int i=k;i<n;++i)
+#define rloop(i,n,k) for(int i=n;i>=k;i--)
+#define vi vector<int>
+#define vvi vector<vector<int> >
+#define vpii vector<pair<int,int> >
+#define all(v) v.begin(),v.end()
+#define mset(a,i) memset(a,i,sizeof(a))
+#define mii map<int,int>  
+#define pii pair<int,int>
+#define deb(...) __f(#__VA_ARGS__, __VA_ARGS__)
+#define mp make_pair
+#define pb push_back
+#define mod 1000000007  
+#define endl "\n"
+#define inf 1e18
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { if(0) return; cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args){   
+    if(0) return; 
+    const char* comma = strchr (names + 1, ',');
+    cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+void get_it_done(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+int gcd(int a, int b){ 
+    if (a == 0) 
+        return b; 
+    return gcd(b % a, a); 
+} 
+const int N = 3e5+5;
+vpii adjList[N];
+int n,e;
+int dist[N];
+// int bellmanFord(int src){
+//     loop(i,0,n+1) dist[i] = inf;
+//     dist[1] = 0;
+//     loop(i,0,n-1){
+
+//     }
+// }
+
+int32_t main(){
+    get_it_done();
+    int t=1;
+    // cin >> t;
+    while (t--){
+        cin>>n>>e;
+        loop(i,0,e){
+            int u,v,w;
+            cin>>u>>v>>w;
+            adjList[u].pb(mp(v,w));
+            adjList[v].pb(mp(u,w));
+        }                
+        
+    }
+}
